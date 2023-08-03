8951a9814dc587b547e8734b25464beb883de4bc
闵逢雨 <5111234+minfengyu@user.noreply.gitee.com>
2023-08-03 13:26:41 +0800

    band check ts rules

--- next.config.js
+++ next.config.js
@@ -6,6 +6,17 @@ const nextConfig = {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
   },
+  eslint: {
+    // 禁用 ESLint
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
